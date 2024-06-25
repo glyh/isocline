a223e18a7574efcf7dd8ebb5889b6b15c79cc96f
glyh <lyhokia@gmail.com>
2024-06-25 19:10:10 +0800

    wrap 2 simple functions

--- wrapper/lib.zig
+++ wrapper/lib.zig
@@ -1,3 +1,13 @@
-pub const isocline = @cImport({
+const isocline = @cImport({
     @cInclude("../include/isocline.h");
 });
+
+pub fn readline(prompt_text: []const u8) ?[]const u8 {
+    return @ptrCast(isocline.ic_readline(@ptrCast(prompt_text)));
+}
+
+pub fn printf(comptime fmt: []const u8, ...) void {
+    var ap = @cVaStart();
+    defer @cVaEnd(&ap);
+    isocline.ic_vprintf(@ptrCast(fmt), ap);
+}
