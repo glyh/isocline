64c0b0bf0fdfc921bfe3ab37399893d29889d3d7
glyh <lyhokia@gmail.com>
2024-06-25 22:12:44 +0800

    expose the interface in the library

--- wrapper/lib.zig
+++ wrapper/lib.zig
@@ -4,7 +4,7 @@ const isocline = @cImport({
 
 const std = @import("std");
 
-pub inline fn readline(prompt_text: []const u8) ?[]const u8 {
+pub export fn readline(prompt_text: []const u8) ?[]const u8 {
     const ptr = @as(?[*:0]u8, isocline.ic_readline(@ptrCast(prompt_text)));
     if (ptr) |result| {
         return std.mem.span(result);
@@ -28,23 +28,23 @@ const IsoclineWriter = struct {
     }
 };
 
-pub inline fn print(comptime fmt: []const u8, args: anytype) !void {
+pub export fn print(comptime fmt: []const u8, args: anytype) !void {
     try std.fmt.format(IsoclineWriter{}, fmt, args);
 }
 
-pub inline fn setHistory(file_name: ?[]const u8, max_entries: c_long) void {
+pub export fn setHistory(file_name: ?[]const u8, max_entries: c_long) void {
     isocline.ic_set_history(@ptrCast(file_name), max_entries);
 }
 
-pub inline fn enableAutoTab(enabled: bool) void {
+pub export fn enableAutoTab(enabled: bool) void {
     isocline.ic_enable_auto_tab(enabled);
 }
 
-pub inline fn historyAdd(entry: []const u8) void {
+pub export fn historyAdd(entry: []const u8) void {
     isocline.ic_history_add(@ptrCast(entry));
 }
 
-pub inline fn styleDef(style_name: []const u8, fmt: []const u8) void {
+pub export fn styleDef(style_name: []const u8, fmt: []const u8) void {
     isocline.ic_style_def(@ptrCast(style_name), @ptrCast(fmt));
 }
 
